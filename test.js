f7df8429e387f89c3ced937c07b404a035dc1620
Matthew Lutze <mlutze@users.noreply.github.com>
2024-06-01 11:58:00 +0200

    add tests

--- test.js
+++ test.js
@@ -2,7 +2,25 @@ function runTests() {
     test("empty counter always 0", () => {
         let c = new Counter();
         assert(c.get("a") === 0);
-        assert(c.get("b") === 0);
     });
 
+    test("adding one sets to 1", () => {
+        let c = new Counter();
+        c.add("a");
+        assert(c.get("a") === 1);
+    })
+
+    test("adding one does not change others", () => {
+        let c = new Counter();
+        c.add("a");
+        assert(c.get("b") === 0);
+    })
+
+    test("adding three sets to 3", () => {
+        let c = new Counter();
+        c.add("a");
+        c.add("a");
+        c.add("a");
+        assert(c.get("a") === 3);
+    })
 }
